def6c3cee764619c6bd6bf836231e01d62f813b6
ghwjd <39ghwjd@naver.com>
2018-11-23 20:17:51 +0900

    mul

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,14 @@
-public class Calculator() {
+public class Calculator{
 
   public int sum(int a, int b) {
     return a + b;
   }
 
-  public int minus(int a, intb ) {
+  public int minus(int a, int b ) {
     return a - b;
   }
 
+  public int multiply(int a, int b){
+
+  }
 }
